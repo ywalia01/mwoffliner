9245d12b9ca4c9da47e6edec26ab33bfbd4715fd
Manan Jethwani <mananjethwani02@gmail.com>
2020-12-09 00:45:24 +0530

    fixing url check for mobile page

--- src/Downloader.ts
+++ src/Downloader.ts
@@ -247,8 +247,8 @@ class Downloader {
     // accordingly. We need to set a default page (always there because
     // installed per default) to request the REST API, otherwise it would
     // fail the check.
-    this.mwCapabilities.mobileRestApiAvailable = await this.checkApiAvailabilty(this.mw.mobileRestApiUrl.href + encodeURIComponent(testArticleId));
-    this.mwCapabilities.desktopRestApiAvailable = await this.checkApiAvailabilty(this.mw.desktopRestApiUrl.href + encodeURIComponent(testArticleId));
+    this.mwCapabilities.mobileRestApiAvailable = await this.checkApiAvailabilty(this.mw.mobileRestApiUrl.href + encodeURIComponent(testArticleId.replace(/ /g,'_') || 'Main_Page'));
+    this.mwCapabilities.desktopRestApiAvailable = await this.checkApiAvailabilty(this.mw.desktopRestApiUrl.href + encodeURIComponent(testArticleId.replace(/ /g,'_')));
     this.mwCapabilities.veApiAvailable = await this.checkApiAvailabilty(this.mw.veApiUrl.href);
 
     // Coordinate fetching
